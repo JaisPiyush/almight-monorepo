79290c1013934905bf8689deee4635cec606586a
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-02-13 18:54:01 +0530

    NoDelegateCall test added

---
+++ test/utils/helpers/NoDelegateCall.t.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "../../../lib/forge-std/src/Test.sol";
+import "../../../protocols/utils/contracts/helpers/NoDelegateCall.sol";
+
+
+contract DummyContract is NoDelegateCall {
+
+    function addSum(uint256 a, uint256 b) external view returns(address, uint256) {
+        return (address(this), a + b);
+    }
+}
+
+
+contract NoDelegateCallTest is Test {
+
+    address public ctr;
+
+    function setUp() public {
+        ctr = address(new DummyContract());
+    }
+
+    function test_withoutDelegateCall() public {
+        (address addr, ) = DummyContract(ctr).addSum(4, 3);
+        assertEq(addr, ctr);
+    }
+
+    function test_withDelegateCall() public  {
+        vm.expectRevert();
+        (bool success, ) = ctr.delegatecall(
+            abi.encodeWithSignature("addSum(uint256,uint256)", 4, 3)
+        );
+        assertFalse(success);
+
+    }
+
+
+}
