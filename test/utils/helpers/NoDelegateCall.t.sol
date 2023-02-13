82020a5580158f6b5cec288f624afea0e56d40b6
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-02-13 20:05:37 +0530

    Module remapping changes

--- test/utils/helpers/NoDelegateCall.t.sol
+++ test/utils/helpers/NoDelegateCall.t.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "forge-std/Test.sol";
+import "@almight/modules/forge-std/src/Test.sol";
 import "../../../protocols/utils/contracts/helpers/NoDelegateCall.sol";
 
 
