6b9c6189d1d1f8848711e72cb6cc43fb8489a9fb
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-02-17 01:48:41 +0530

    Tickerd pool libraries added from UniswapV3

--- protocols/interfaces/contracts/vault/IVault.sol
+++ protocols/interfaces/contracts/vault/IVault.sol
@@ -1,6 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-interface IVault {
 
+import "./ITokenHandler.sol";
+import "./IVaultAuthorizer.sol";
+
+interface IVault is IVaultAuthorizer, ITokenHandler {
+
+    function getAdmin() external view returns(address);
 }
